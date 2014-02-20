fd8d4c22c42deddcb94436b6db537e1dd1ea9dbb
Quildreen "Sorella" Motta <quildreen@gmail.com>
2014-02-20 17:54:02 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+Fuck You
+========
+
+A ragy way of killing yo processes.
+
+![](https://raw.github.com/robotlolita/fuck-you/master/example.gif)
+
+- - -
+MIT licensed.
---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+if (process.argv.length < 4) {
+  console.log('Usage: fuck you [process]')
+  process.exit(1)
+}
+
+var exec     = require('child_process').exec
+var sanitise = JSON.stringify
+var toArray  = [].slice.call.bind([].slice)
+
+var chars   = " -_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+var flipped = " -_ɐqɔpǝɟɓɥıɾʞlɯuodbɹsʇnʌʍxʎz∀𐐒ƆᗡƎℲ⅁HIſ⋊⅂WNOԀΌᴚS⊥∩ΛMX⅄Z⇂ᄅƐㄣގ9ㄥ860"
+var pname   = process.argv[3]
+
+
+exec('killall -9 ' + sanitise(pname), function(error, stdout, stderr) {
+  if (error == null)  show('(╯°□°）╯︵', flip(pname))
+  else                show('(；￣Д￣) . o O( It’s not very effective... )') })
+
+
+function show() {
+  console.log('\n ' + toArray(arguments).join('') + '\n') }
+
+function flip(name) {
+  return name.toLowerCase()
+             .split('')
+             .reverse()
+             .join('')
+             .replace(/./g, function(a) {
+                              var i = chars.indexOf(a)
+                              return i != -1?  flipped[i]
+                              :      /* _ */   '' })}
