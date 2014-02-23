8b50ca611c30a215dbc9db33c0ae2cced9e51cdc
Quildreen "Sorella" Motta <quildreen@gmail.com>
2014-02-23 13:59:43 -0300

    Don't kill the current process (Fixes #2)

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 #!/usr/bin/env node
 
-if (process.argv.length < 4) {
-  console.log('Usage: fuck you [process]')
-  process.exit(1)
-}
-
 var exec     = require('child_process').exec
+var λ        = require('core.lambda')
+var Future   = require('data.future')
+var sequence = require('control.monads').sequence
 var sanitise = JSON.stringify
 var toArray  = [].slice.call.bind([].slice)
 
-var chars   = " -_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-var flipped = " -_ɐqɔpǝɟɓɥıɾʞlɯuodbɹsʇnʌʍxʎz∀𐐒ƆᗡƎℲ⅁HIſ⋊⅂WNOԀΌᴚS⊥∩ΛMX⅄Z⇂ᄅƐㄣގ9ㄥ860"
-var pname   = process.argv[3]
+var chars       = " -_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+var flipped     = " -_ɐqɔpǝɟɓɥıɾʞlɯuodbɹsʇnʌʍxʎz∀𐐒ƆᗡƎℲ⅁HIſ⋊⅂WNOԀΌᴚS⊥∩ΛMX⅄Z⇂ᄅƐㄣގ9ㄥ860"
+
+main(process.argv, process.pid)
 
 
-exec('killall -9 ' + sanitise(pname), function(error, stdout, stderr) {
-  if (error == null)  show('(╯°□°）╯︵', flip(pname))
-  else                show('(；￣Д￣) . o O( It’s not very effective... )') })
+// :: [String], Number -> () *Eff*
+function main(args, pid) {
+  if (args.length < 4) {
+    show('Usage: fuck you [process]')
+    process.exit(1) }
+
+  var processName = args[3]
+  var processes   = shell('pgrep', [processName])
+  var toKill      = processes.map(function(data) {
+                                    return parseIds(data.output).filter(notEqual(pid))
+                                                                .map(kill) })
+
+  toKill.chain(sequence(Future))
+        .fork( function(e)  { show('(；￣Д￣) . o O( It’s not very effective... )') }
+             , function(xs) { show('(╯°□°）╯︵', flip(processName), ' (x', xs.length, ')') })}
 
 
+// :: String... -> () *Eff*
 function show() {
   console.log('\n ' + toArray(arguments).join('') + '\n') }
 
+
+// :: String -> String
 function flip(name) {
   return name.toLowerCase()
              .split('')
@@ -31,3 +45,29 @@ function flip(name) {
                               var i = chars.indexOf(a)
                               return i != -1?  flipped[i]
                               :      /* _ */   '' })}
+
+
+// :: String, [String] -> Future(Error, { output: String, error: String })
+function shell(command, args) {
+  return new Future(function(reject, resolve) {
+                      exec( command + ' ' + args.map(λ.compose(String)(sanitise)).join(' ')
+                          , function(error, stdout, stderr) {
+                              if (error == null)  resolve({ output: stdout, error: stderr })
+                              else                reject(error) })})}
+
+
+// :: String -> [Number]
+function parseIds(text) {
+  return text.trim()
+             .split(/\s+/)
+             .map(Number) }
+
+
+// :: 'a -> 'a -> Boolean
+function notEqual(x){ return function(y) {
+  return x !== y }}
+
+
+// :: Number -> Future(Error, { output: String, error: String })
+function kill(pid) {
+  return shell('kill', ['-9', pid]) }
